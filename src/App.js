1f8f7708e35f1d31a1aca61e7cc2421da9f890d4
이성일 <kw8989@naver.com>
2021-01-27 18:47:28 +0900

    LoginPage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import LoginPage from './component/LoginPage.js'
+import './App.css'
+
+class App extends Component{
+  render(){
+    return(
+        <LoginPage/>
+    );
+  }
+}
+
+export default App;
