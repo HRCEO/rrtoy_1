e02b62b3662a4e72f9da164dcab0760a131ba037
이성일 <kw8989@naver.com>
2021-02-09 17:59:37 +0900

    react study state 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import LoginPage from './component/LoginPage.js'
 import MyComponent from './reactStudy/MyComponent.js'
 import MyComponentClass from "./reactStudy/MyComponentClass";
+import Counter from "./reactStudy/Counter";
 import './App.css'
 
 class App extends Component{
   render(){
     return(
         <div>
+            <Counter fixedNumber={2}/>
             {/*<MyComponent name={'청이'} favoriteNumber={1}>리액트</MyComponent>*/}
             {/*<MyComponentClass name={'청이'} favoriteNumber={1}>리액트</MyComponentClass>*/}
             {/*<LoginPage/>*/}
