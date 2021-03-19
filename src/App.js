9f39c3e8c9ba33cbcdb4727337115279bf038e5e
이성일 <kw8989@naver.com>
2021-03-19 19:33:12 +0900

    react toy project-loginpage made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react'
 import LoginPage from './component/LoginPage.js'
-import MyComponent from './reactStudy/MyComponent.js'
-import MyComponentClass from "./reactStudy/MyComponentClass";
-import Counter from "./reactStudy/Counter";
-import Say from "./reactStudy/useState"
-import EventPractice from "./reactStudy/EventPractice";
-import EventPracticeF from "./reactStudy/EventPracticeF"
-// import DomComponent from "./reactStudy/DomComponent";
-import ScrollBox from "./reactStudy/ScrollBox";
 
 import './App.css'
 
@@ -15,16 +7,7 @@ class App extends Component{
   render(){
     return(
         <div>
-            {/*<Counter fixedNumber={2}/>*/}
-            {/*<MyComponent name={'청이'} favoriteNumber={1}>리액트</MyComponent>*/}
-            {/*<MyComponentClass name={'청이'} favoriteNumber={1}>리액트</MyComponentClass>*/}
-            {/*<LoginPage/>*/}
-            {/*<Say/>*/}
-            {/*<EventPractice/>*/}
-            {/*<EventPracticeF/>*/}
-            {/*<DomComponent/>*/}
-            {/*<ScrollBox ref={(ref)=>this.scrollBox=ref}/>*/}
-            {/*<button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>*/}
+            <LoginPage/>
         </div>
     );
   }
--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -3,16 +3,15 @@ import BackGroundVideo from '../video/main_login.mp4';
 import '../css/LoginPage.css'
 
 class LoginPage extends Component {
-    text;
-    render(){
+    render() {
         return (
             <div>
                 <video autoPlay loop muted
                        style={{
-                           position:'absolute',
+                           position: 'absolute',
                            width: '100vw',
                            height: '100vh',
-                           objectFit : "cover",
+                           objectFit: "cover",
                            right: "0px",
                            left: "0px",
                            top: "0px",
@@ -27,13 +26,15 @@ class LoginPage extends Component {
                         <div className="Login_from">
                             <div className="Login_Row">
                                 <svg className="Login_icon" viewBox="0 0 20 20">
-                                    <path className="Login_path" d="M0,20 a10,8 0 0,1 20,0z M10,0 a4,4 0 0,1 0,8 a4,4 0 0,1 0,-8"/>
+                                    <path className="Login_path"
+                                          d="M0,20 a10,8 0 0,1 20,0z M10,0 a4,4 0 0,1 0,8 a4,4 0 0,1 0,-8"/>
                                 </svg>
                                 <input type="text" className="login_input_id" placeholder="Username"/>
                             </div>
                             <div className="Login_Row">
                                 <svg className="Login_icon" viewBox="0 0 20 20">
-                                    <path className="Login_path" d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0"/>
+                                    <path className="Login_path"
+                                          d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0"/>
                                 </svg>
                                 <input type="password" className="login_input_pw" placeholder="Password"/>
                             </div>
@@ -47,5 +48,6 @@ class LoginPage extends Component {
         );
     }
 }
+
 export default LoginPage;
 
